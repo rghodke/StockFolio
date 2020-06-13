150c7d16029bee4fcbd68a13c44bac600622d33d
Ranjit Ghodke <ranjitghodke@yahoo.com>
2020-06-13 15:12:48 -0700

    improvement(stockfolio): Overhaul Add Asset Fragment

--- app/src/main/java/com/clearbox/stockfolio/fragment/AddAssetFragment.java
+++ app/src/main/java/com/clearbox/stockfolio/fragment/AddAssetFragment.java
@@ -11,6 +11,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.clearbox.stockfolio.R;
 import com.clearbox.stockfolio.adapter.MyAssetItemRecyclerViewAdapter;
@@ -21,11 +24,6 @@ import com.clearbox.stockfolio.fragment.dummy.DummyContent;
  */
 public class AddAssetFragment extends Fragment {
 
-    // TODO: Customize parameter argument names
-    private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
-    private int mColumnCount = 1;
-
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
@@ -33,7 +31,6 @@ public class AddAssetFragment extends Fragment {
     public AddAssetFragment() {
     }
 
-    // TODO: Customize parameter initialization
     @SuppressWarnings("unused")
     public static AddAssetFragment newInstance() {
         AddAssetFragment fragment = new AddAssetFragment();
@@ -43,28 +40,34 @@ public class AddAssetFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add_asset, container, false);
+        setupViews(view);
 
-        // Set the adapter
-        if (view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
-            recyclerView.setAdapter(new MyAssetItemRecyclerViewAdapter(DummyContent.ITEMS));
-        }
         return view;
     }
+
+    private void setupViews(View view) {
+        final Context context = view.getContext();
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.RecyclerView_Add_Asset);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        recyclerView.setAdapter(new MyAssetItemRecyclerViewAdapter(DummyContent.ITEMS));
+
+        final EditText mSearchEditText = view.findViewById(R.id.EditText_Search);
+        mSearchEditText.requestFocus();
+        mSearchEditText.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                InputMethodManager keyboard = (InputMethodManager)
+                        context.getSystemService(Context.INPUT_METHOD_SERVICE);
+                keyboard.showSoftInput(mSearchEditText, 0);
+            }
+        },200);
+    }
+
 }
